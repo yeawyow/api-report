0c3c3ef0860bd791f094066c737ba06bc9f0590d
yeawyow <yeawyow@gmail.com>
2021-02-02 16:11:01 +0700

    tttt

--- model/Employee.go
+++ model/Employee.go
@@ -1,14 +1,14 @@
 package model
 
 type Employee struct {
-	UID                uint
-	Username           string
-	Pass               string
-	PnameID            int
-	Fname              string
-	Lname              string
-	GroupUserID        int
-	HospitalDepartment int
-	PositionID         int
-	Status             string
+	UID                uint   `json:"uid"`
+	Username           string `json:"username"`
+	Pass               string `json:"pass"`
+	PnameID            int    `json:"pname_id"`
+	Fname              string `json:"fname"`
+	Lname              string `json:"lname"`
+	GroupUserID        int    `json:"group_user_id"`
+	HospitalDepartment int    `json:"hospital_department"`
+	PositionID         int    `json:"position_id"`
+	Status             string `json:"status"`
 }
