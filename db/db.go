b3aaff340a9613db32e2353496d572d43fa0767b
yeawyow <yeawyow@gmail.com>
2021-02-03 13:15:13 +0700

    table name ok

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"gorm.io/driver/mysql"
+	"gorm.io/gorm/schema"
 
 	//"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -15,8 +16,11 @@ func GetDB() *gorm.DB{
 func SetupDB(){
 
 	dsn := "yeaw:481725209@tcp(202.80.228.46:3306)/airoffice?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	database,err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, //nochang table name
+		},
+	})
 
 	//database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 
--- model/Employee.go
+++ model/Employee.go
@@ -1,14 +1,6 @@
 package model
 
 type Employee struct {
-	UID                uint   `json:"uid"`
-	Username           string `json:"username"`
-	Pass               string `json:"pass"`
-	PnameID            int    `json:"pname_id"`
-	Fname              string `json:"fname"`
-	Lname              string `json:"lname"`
-	GroupUserID        int    `json:"group_user_id"`
-	HospitalDepartment int    `json:"hospital_department"`
-	PositionID         int    `json:"position_id"`
-	Status             string `json:"status"`
+	UID      int
+	Username string
 }
